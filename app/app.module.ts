0984b20e1c29df7af5daef1a2674095f3484525c
Shengzhi Wang <szwang@thoughtworks.com>
2017-01-18 14:53:36 +0800

    use HeroesComponent to do the hero jobs

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,11 +3,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule}   from '@angular/forms';
 
 import {AppComponent}  from './app.component';
-import {HeroDetailComponent} from './hero-detail.component'
+import {HeroDetailComponent} from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent,HeroDetailComponent],
+  declarations: [AppComponent,HeroDetailComponent,HeroesComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -2,10 +2,16 @@ import {Injectable} from '@angular/core';
 
 import {Hero} from './Hero';
 import {HEROES} from './mock-heroes';
+import resolve = require("resolve");
 
 @Injectable()
 export class HeroService {
     getHeroes():Hero[] {
         return HEROES;
+    }
+    getHeroesSlowly(): Promise<Hero[]> {
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(resolve, 2000))
+            .then(() => this.getHeroes());
     }
 }
